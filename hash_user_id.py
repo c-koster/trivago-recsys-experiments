95a212eb84ea6b5aec67823e2e8b8072f947584a
c-koster <52757828+c-koster@users.noreply.github.com>
2021-07-16 15:02:42 -0400

    file to generate a new dataset for experiment 2

---
+++ hash_user_id.py
@@ -0,0 +1,29 @@
+"""
+Second experiment:
+
+We think our user-aware recommender system is pretty powerful. But what happens when we pretend ?
+
+
+This will be annswered in the second experiment file -- but first we have to make identical
+train/test.csv files with re-coded user_ids to pretend that each session comes from a 'new
+user', aka 1 user id per 1 session.
+
+Do this by setting the user id to the hash of the session_id.
+"""
+from hashlib import md5
+import pandas as pd
+
+def hash_session(id: str) -> str:
+    """ Input a session id and perform md5, then return a string of the hex digest. """
+    bytes = id.encode('utf-8')
+    return md5(bytes).hexdigest()
+
+
+def convert_file(what: str) -> None:
+    assert what in ["train","test"]
+    df = pd.read_csv("data/trivago/{}.csv".format(what))
+    df["user_id"] = df["session_id"].apply(hash_session)
+    df.to_csv("data/trivago/{}_hashed.csv".format(what),index=False)
+
+convert_file("train")
+convert_file("test")
