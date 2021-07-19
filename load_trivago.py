8bbfc7c1718f10846c546ae0bd2230626550bbee
c-koster <52757828+c-koster@users.noreply.github.com>
2021-07-19 15:56:37 -0400

    nrows ...

--- load_trivago.py
+++ load_trivago.py
@@ -280,8 +280,8 @@ def load_session_dict(what: str) -> Dict[str,Session]:
     assert(what in ["train","test"])
 
     sessions: List[Session] = []
-    # nrows=1_000 for my laptop's sake
-    df_interactions = pd.read_csv("data/trivago/{}.csv".format(what),nrows=1_000) #type:ignore
+    # s=1_000 for my laptop's sake
+    df_interactions = pd.read_csv("data/trivago/{}.csv".format(what)) #type:ignore
     # appply the "save_session" function to each grouped item/session
     # but first turn each group from a df into a list of dictionaries
     A = lambda x: sessions.append(create_session(x.to_dict("records"))) #type:ignore
